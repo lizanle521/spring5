327a1e8566a31e51664da9842ae4f93de1035560
lizanle <491823689@qq.com>
2019-01-15 14:47:48 +0800

    多配置文件

---
+++ spring-boot-multi-profile/src/main/java/com/example/MultiConfigApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lizanle
+ * @data 2019/1/15 下午2:28
+ */
+@SpringBootApplication
+public class MultiConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiConfigApplication.class,args);
+    }
+}
