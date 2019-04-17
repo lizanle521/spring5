c201d7b825f10de4b42a76f487eef2e9497a34ce
lizanle <491823689@qq.com>
2019-04-17 18:01:44 +0800

    hystrixdemo

---
+++ spring-boot-hystrix-demo/src/main/java/com/lzl/hystrix/Application.java
@@ -0,0 +1,15 @@
+package com.lzl.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lizanle
+ * @Date 2019/4/17 17:29
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
